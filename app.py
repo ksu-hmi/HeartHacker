004acdc7a4763a7209c4043e7e4fa84cba7bca88
Caroline Lazaro <clazaro@students.kennesaw.edu>
2023-11-02 23:43:52 -0400

    Includes explanation for code

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #Importing the necessary libraries pyaudio for audiorecording, numpy for data manipulation, and wavio for saving the recorded audio 
 import pyaudio
 import numpy as np
-import wavio  # Import the wavio library
+import wavio  
+ 
+# Constants for audio recording including sample rate and the recording duration
 
-# Constants for audio recording
 SAMPLE_RATE = 44100  # Sample rate in Hz
-RECORDING_DURATION = 10  # Duration of recording in seconds
+RECORDING_DURATION = 5  # Duration of recording in seconds
 
 #Creating the record_audio function- it initializes a PyAudio object, opens an audio stream with the specified parameters, records audio data for a specified duration and saves it in a list called 'frames'
 def record_audio():
@@ -16,6 +17,8 @@ def record_audio():
                         rate=SAMPLE_RATE,
                         input=True,
                         frames_per_buffer=1024)
+    
+    #Recording process: The code prints "Reocording" to indicate that audio recording has started. It records the audio in chunks for the specified duration and appends each chunk to the frames list
 
 #Recording process: The code prints "Reocording" to indicate that audio recording has started. It records the audio in chunks for the specified duration and appends each chunk to the frames list
     print("Recording...")
@@ -27,6 +30,8 @@ def record_audio():
 
     print("Recording finished.")
 
+    #After recording is finished, the script stops the audio stream, closes it, and terminates the PyAudio object
+
 #After recording is finished, the script stops the audio stream, closes it, and terminates the PyAudio object
     stream.stop_stream()
     stream.close()
@@ -34,12 +39,9 @@ def record_audio():
 
     return np.frombuffer(b''.join(frames), dtype=np.int16)
 
-#The code then saves the recorded audio to a WAV file named "recorded_audio.wav" in the same directory as the script using the wavio.write function
 if __name__ == "__main__":
     audio_data = record_audio()
 
 # Save the recorded audio to a WAV file
     wavio.write("recorded_audio.wav", audio_data, SAMPLE_RATE, sampwidth=2)
 
-# Process and analyze audio_data for heart rate detection
-# Implement heart rate detection algorithm and visualization here
