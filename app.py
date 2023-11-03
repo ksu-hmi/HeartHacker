345053e00902e8e4bf9a0a52e28cf21cc7bd1a97
Caroline Lazaro <137337239+lazarocf@users.noreply.github.com>
2023-11-02 23:53:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Importing the necessary libraries pyaudio for audiorecording, numpy for data manipulation, and wavio for saving the recorded audio 
 import pyaudio
 import numpy as np
 import wavio  # Import the wavio library
@@ -6,6 +7,7 @@ import wavio  # Import the wavio library
 SAMPLE_RATE = 44100  # Sample rate in Hz
 RECORDING_DURATION = 5  # Duration of recording in seconds
 
+#Creating the record_audio function- it initializes a PyAudio object, opens an audio stream with the specified parameters, records audio data for a specified duration and saves it in a list called 'frames'
 def record_audio():
     audio = pyaudio.PyAudio()
 
@@ -15,6 +17,7 @@ def record_audio():
                         input=True,
                         frames_per_buffer=1024)
 
+#Recording process: The code prints "Reocording" to indicate that audio recording has started. It records the audio in chunks for the specified duration and appends each chunk to the frames list
     print("Recording...")
     frames = []
 
@@ -24,17 +27,19 @@ def record_audio():
 
     print("Recording finished.")
 
+#After recording is finished, the script stops the audio stream, closes it, and terminates the PyAudio object
     stream.stop_stream()
     stream.close()
     audio.terminate()
 
     return np.frombuffer(b''.join(frames), dtype=np.int16)
 
+#The code then saves the recorded audio to a WAV file named "recorded_audio.wav" in the same directory as the script using the wavio.write function
 if __name__ == "__main__":
     audio_data = record_audio()
 
-    # Save the recorded audio to a WAV file
+# Save the recorded audio to a WAV file
     wavio.write("recorded_audio.wav", audio_data, SAMPLE_RATE, sampwidth=2)
 
-    # Process and analyze audio_data for heart rate detection
-    # Implement heart rate detection algorithm and visualization here
+# Process and analyze audio_data for heart rate detection
+# Implement heart rate detection algorithm and visualization here
