5e023e0487031bc2c609dc3586e4a2bdfc641fb1
Caroline Lazaro <137337239+lazarocf@users.noreply.github.com>
2023-11-02 23:54:51 -0400

    Update app.py
    Changing the duration of the audio to 10 seconds

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import wavio  # Import the wavio library
 
 # Constants for audio recording
 SAMPLE_RATE = 44100  # Sample rate in Hz
-RECORDING_DURATION = 5  # Duration of recording in seconds
+RECORDING_DURATION = 10  # Duration of recording in seconds
 
 #Creating the record_audio function- it initializes a PyAudio object, opens an audio stream with the specified parameters, records audio data for a specified duration and saves it in a list called 'frames'
 def record_audio():
