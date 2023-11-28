cb864d7a68dbe62691f4c7f3d9bbf2fe0c769919
Caroline Lazaro <clazaro@students.kennesaw.edu>
2023-11-28 18:40:15 -0500

    Update app.py
    Fixing resizing error

--- app.py
+++ app.py
@@ -229,15 +229,17 @@ root.title("Heart Rate Recorder")
 logo_url = 'https://raw.githubusercontent.com/ksu-hmi/HeartHacker/main/HeartHacker.png'
 logo_image_bytes = urlopen(logo_url, context=ssl_context).read()
 logo_image = Image.open(BytesIO(logo_image_bytes))
+
 # Setting desired width and height for resizing
 width, height = 200, 200
-logo_image = logo_image.resize((width, height), Image.ANTIALIAS)
-logo_image = ImageTk.PhotoImage(logo_image)
+logo_image = logo_image.resize((width, height), Image.ANTIALIAS)  # Use 'ANTIALIAS' in uppercase
 
+logo_image = ImageTk.PhotoImage(logo_image)
 
 # Create a label for the logo image
 logo_label = tk.Label(root, image=logo_image)
 logo_label.pack(pady=10)
+
 
 # Create a button to start recording
 start_button = tk.Button(root, text="Recording", command=start_recording)
