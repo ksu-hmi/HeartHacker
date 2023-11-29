53fae3e09e69a003751b0de72df5f305b420f58f
Caroline Lazaro <clazaro@students.kennesaw.edu>
2023-11-28 20:25:02 -0500

    Update app.py
    Resizing the buttons and moving so they are beside each other

--- app.py
+++ app.py
@@ -249,13 +249,18 @@ restart_button_image = ImageTk.PhotoImage(restart_button_image)
 logo_label = tk.Label(root, image=logo_image)
 logo_label.pack(pady=10)
 
+# Resize the images for buttons
+width, height = 50, 50  # Adjust the width and height as needed
+recording_button_image = ImageTk.PhotoImage(recording_button_image.resize((width, height)))
+restart_button_image = ImageTk.PhotoImage(restart_button_image.resize((width, height)))
+
 # Create a button to start recording
-start_button = tk.Button(root, text="Recording", command=start_recording)
-start_button.pack(pady=10)
+start_button = tk.Button(root, image=recording_button_image, command=start_recording)
+start_button.pack(side=tk.LEFT, padx=5)
 
 # Create a button to restart the process
-restart_button = tk.Button(root, text="Restart", command=restart_process)
-restart_button.pack(pady=10)
+restart_button = tk.Button(root, image=restart_button_image, command=restart_process)
+restart_button.pack(side=tk.LEFT, padx=5)
 
 # Create a label to display the result
 result_label = tk.Label(root, text="")
